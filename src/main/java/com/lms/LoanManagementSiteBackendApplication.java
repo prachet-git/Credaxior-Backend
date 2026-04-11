ec21c2ebcfe51114551b8dbc1ebc1b059df898f0
Prachet <prachetm8@gmail.com>
2026-04-11 10:46:56 +0530

    Tested password encryption

--- src/main/java/com/lms/LoanManagementSiteBackendApplication.java
+++ src/main/java/com/lms/LoanManagementSiteBackendApplication.java
@@ -3,11 +3,15 @@ package com.lms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class LoanManagementSiteBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanManagementSiteBackendApplication.class, args);
 	}
+	
+	
 
 }
